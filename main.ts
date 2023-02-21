b7919728e8fff8f9c18189a517bfc8b9085267a5
LPlenniHD <45006238+LPlenniHD@users.noreply.github.com>
2023-02-21 11:05:25 +0100

    Made a couple of Changes so now you can use a second microbit as an Remote

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let Mode = 0
 let Power = 0
 let ALARM = 0
+let Alarm1 = 0
 input.onSound(DetectedSound.Loud, function () {
     if (Mode == 1) {
         if (Power == 1) {
@@ -10,31 +11,6 @@ input.onSound(DetectedSound.Loud, function () {
         } else {
             ALARM = 0
         }
-    }
-})
-input.onButtonPressed(Button.A, function () {
-    if (Mode == 0) {
-        Mode = 1
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            . # . # .
-            . . # . .
-            . # # # .
-            `)
-        basic.pause(100)
-        basic.clearScreen()
-    } else {
-        Mode = 0
-        basic.showLeds(`
-            . # . # .
-            # . # . #
-            . # # # .
-            # . # . #
-            . # . # .
-            `)
-        basic.pause(100)
-        basic.clearScreen()
     }
 })
 input.onGesture(Gesture.Shake, function () {
@@ -49,47 +25,83 @@ input.onGesture(Gesture.Shake, function () {
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    if (Power == 0) {
-        Power = 1
-        basic.showIcon(IconNames.Yes)
-        basic.pause(100)
-        basic.clearScreen()
-    } else {
-        Power = 0
-        basic.showIcon(IconNames.No)
-        basic.pause(100)
+    radio.setGroup(192)
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "HeLl0") {
+        if (Power == 0) {
+            Power = 1
+            basic.showIcon(IconNames.Yes)
+            basic.pause(100)
+            basic.clearScreen()
+        } else {
+            Power = 0
+            basic.showIcon(IconNames.No)
+            basic.pause(100)
+            basic.clearScreen()
+        }
+    }
+    if ("G0oD" == receivedString) {
+        if (Mode == 0) {
+            Mode = 1
+            basic.showLeds(`
+                . . # . .
+                . # . # .
+                . # . # .
+                . . # . .
+                . # # # .
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+        } else {
+            Mode = 0
+            basic.showLeds(`
+                . # . # .
+                # . # . #
+                . # # # .
+                # . # . #
+                . # . # .
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+        }
+    }
+    if ("A1arM" == receivedString) {
+        ALARM = 0
+        music.stopAllSounds()
+        music.stopMelody(MelodyStopOptions.All)
         basic.clearScreen()
     }
 })
 basic.forever(function () {
     if (ALARM == 1) {
-        for (let index = 0; index < 15; index++) {
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        Alarm1 += 1
+        if (Alarm1 == 15) {
+            ALARM = 0
         }
     } else {
-    	
+        ALARM = 0
     }
 })
 basic.forever(function () {
     if (ALARM == 1) {
-        for (let index = 0; index < 60; index++) {
-            music.playTone(659, music.beat(BeatFraction.Half))
-            music.playTone(587, music.beat(BeatFraction.Half))
-        }
+        music.playTone(659, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Half))
     } else {
-    	
+        ALARM = 0
     }
 })
